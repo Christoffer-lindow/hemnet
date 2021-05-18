4d00e81f330eed24029096ed9469e0efcc0bc798
Christoffer-lindow <clindow@gmail.com>
2021-05-18 19:17:17 +0700

    Moved the sidebar to its own component, rendering of components are still done by the Home view

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="sidebar">
+    <div
+      class="sidebar-btn"
+      :class="activeLink == 'overview' && 'active'"
+      @click="sidebarClicked('overview')"
+    >
+      Overview
+    </div>
+    <div
+      class="sidebar-btn"
+      :class="activeLink == 'gavle_dataset' && 'active'"
+      @click="sidebarClicked('gavle_dataset')"
+    >
+      Gävle Dataset
+    </div>
+    <div
+      class="sidebar-btn"
+      :class="activeLink == 'stockholm_dataset' && 'active'"
+      @click="sidebarClicked('stockholm_dataset')"
+    >
+      Stockholm Dataset
+    </div>
+    <div
+      class="sidebar-btn"
+      :class="
+        activeLink == 'property_description' ? 'active' : 'sidebar-btn-disabled'
+      "
+    >
+      Property Description
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["active-link"],
+    methods: {
+        sidebarClicked(component) {
+            this.$emit("sidebarClicked", component)
+        }
+    }
+};
+</script>
+
+<style>
+/* The side navigation menu */
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 225px;
+  background-color: white;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+.sidebar-btn {
+    width: 100%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    cursor: pointer;
+}
+.active {
+    background-color: green;
+    color: white;
+}
+/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+div.content {
+  margin-left: 225px;
+  height: 1000px;
+}
+
+/* On screens that are less than 700px wide, make the sidebar into a topbar */
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar-btn {
+    float: left;
+  }
+  div.content {
+    margin-left: 0;
+  }
+}
+
+/* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+@media screen and (max-width: 400px) {
+  .sidebar-btn {
+    text-align: center;
+    float: none;
+  }
+}
+.sidebar-btn-disabled {
+    cursor: default;
+    color: #b9b9b9
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
-    <div class="sidebar">
-        <div class="sidebar-btn" :class="activeLink == 'overview' && 'active'" @click="setActive('overview')">Overview</div>
-        <div class="sidebar-btn" :class="activeLink == 'gavle_dataset' && 'active'" @click="setActive('gavle_dataset')">Gävle Dataset</div>
-        <div class="sidebar-btn" :class="activeLink == 'stockholm_dataset' && 'active'" @click="setActive('stockholm_dataset')">Stockholm Dataset</div>
-        <div class="sidebar-btn" :class="activeLink == 'property_description' ? 'active' : 'sidebar-btn-disabled'">Property Description</div>
-    </div>
+      <side-bar v-on:sidebarClicked="handleSidebarClicked" :active-link="activeLink" />
     <div class="content">
       <property-table :city="'gavle'" v-if="activeLink == 'gavle_dataset'" v-on:tableClicked="handleTableClicked" />
       <property-table :city="'stockholm'" v-if="activeLink == 'stockholm_dataset'" v-on:tableClicked="handleTableClicked" />
@@ -17,11 +12,13 @@
 <script>
 import PropertyTable from "../components/PropertyTable.vue";
 import PropertyDescription from "../components/PropertyDescription.vue"
+import SideBar from "../components/Sidebar.vue"
 export default {
   name: "Home",
   components: {
     PropertyTable,
     PropertyDescription,
+    SideBar
   },
   data() {
       return {
@@ -36,61 +33,13 @@ export default {
       handleTableClicked(property) {
           this.property = property
           this.setActive("property_description")
+      },
+      handleSidebarClicked(component) {
+          this.setActive(component)
       }
   }
 };
 </script>
 <style  scoped>
-/* The side navigation menu */
-.sidebar {
-  margin: 0;
-  padding: 0;
-  width: 225px;
-  background-color: white;
-  position: fixed;
-  height: 100%;
-  overflow: auto;
-}
-.sidebar-btn {
-    width: 100%;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    cursor: pointer;
-}
-.active {
-    background-color: green;
-    color: white;
-}
-/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
-div.content {
-  margin-left: 225px;
-  height: 1000px;
-}
 
-/* On screens that are less than 700px wide, make the sidebar into a topbar */
-@media screen and (max-width: 700px) {
-  .sidebar {
-    width: 100%;
-    height: auto;
-    position: relative;
-  }
-  .sidebar-btn {
-    float: left;
-  }
-  div.content {
-    margin-left: 0;
-  }
-}
-
-/* On screens that are less than 400px, display the bar vertically, instead of horizontally */
-@media screen and (max-width: 400px) {
-  .sidebar-btn {
-    text-align: center;
-    float: none;
-  }
-}
-.sidebar-btn-disabled {
-    cursor: default;
-    color: #b9b9b9
-}
 </style>
